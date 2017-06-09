32d6b1830fbff662b8b94d496282a478a3176c4a
ERAU MSSA Student <ERAU MSSA Student>
2017-06-09 09:40:23 -0700

    Add project files.

---
+++ Randomizer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Randomizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[13] { "Jeff", "Angela", "Chris R.", "Ethan", "Mike", "Justin", "Jessica", "Amanda", "Chris L.", "Brian", "Chris M.", "Juan", "Richard" };
+            while (true)
+            {
+                Random selector = new Random();
+                int randomNum = selector.Next(0, 13);
+                string result = names[randomNum];
+                Console.WriteLine(result);
+                Console.WriteLine("Press \"ENTER\" to continue, or type \"quit\" to exit: ");
+                string entry = Console.ReadLine().ToLower();
+                Console.Clear();
+
+
+                if (entry == "quit")
+                {
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
